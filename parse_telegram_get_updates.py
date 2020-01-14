2657177589b45bb950d3e9efe7a1bace36d22db7
Roman Shekhovtsov <rshekhovtsov@drclinics.ru>
2020-01-14 16:07:27 +0300

    python3 replaces with python

--- parse_telegram_get_updates.py
+++ parse_telegram_get_updates.py
@@ -2,10 +2,4 @@ import sys, json
 
 for update in json.load(sys.stdin)['result']:
     from_=update['message']['from']
-    print(
-	from_.get('id','<undefined>'),
-	from_.get('first_name','<undefined>'),
-	from_.get('last_name','<undefined>'),
-	from_.get('username','<undefined>')
-    )
-
+    print from_.get('id','<undefined>'), from_.get('first_name','<undefined>'),	from_.get('last_name','<undefined>'), from_.get('username','<undefined>')
