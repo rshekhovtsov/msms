d1867bfa43ff66e47a040b7ed53f9884738e57b2
Roman Shekhovtsov <41947356+rshekhovtsov@users.noreply.github.com>
2020-01-15 18:45:14 +0300

    Delete parse_telegram_get_updates.py

--- parse_telegram_get_updates.py
+++
@@ -1,5 +0,0 @@
-import sys, json
-
-for update in json.load(sys.stdin)['result']:
-    from_=update['message']['from']
-    print from_.get('id','<undefined>'), from_.get('first_name','<undefined>'),	from_.get('last_name','<undefined>'), from_.get('username','<undefined>')
